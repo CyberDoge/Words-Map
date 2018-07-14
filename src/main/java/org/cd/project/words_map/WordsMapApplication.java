7536aa7f11b56cd70555dee1865a2e453688baf8
CyberDoge <tagirman998@gmail.com>
2018-07-14 17:51:46 +0400

    finish fixes

--- src/main/java/org/cd/project/words_map/WordsMapApplication.java
+++ src/main/java/org/cd/project/words_map/WordsMapApplication.java
@@ -2,11 +2,9 @@ package org.cd.project.words_map;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 
-@ComponentScan("org.cd.project.words_map")
-@SpringBootApplication
+    @SpringBootApplication
 public class WordsMapApplication {
 
     public static void main(String[] args) {
